4d42e48ac179de5bd6c4581e03158107f87485c8
Bruno Bigras <bigras.bruno@gmail.com>
2019-03-19 10:35:33 -0400

    test fix auto raise windows

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "e9a476244df7a8886fc6fc6785251198ed76e601";
-  sha256 = "1qc8ddpmmnqy0ws1kmx6lzq5fvv54j9jimvq05zm9clfv8v14h9m";
-  revdate = "2019-03-18T15:29:19Z";
+  rev = "0adb95490df142928322de349be7605660fd9f98";
+  sha256 = "14la0mw15gij91xzl4riz6kj5y49xwlcckx1r8xcgh9yvv03rxfp";
+  revdate = "2019-03-19T19:41:24+10:00";
 }
